49e4c41643e00f428a3d03df85667ccb754034b8
Nolan <nkuza8@gmail.com>
2019-11-30 12:35:25 -0500

    Create RDP.bat

---
+++ RDP.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:a
+set /p rdpChk="Enable remote desktop (y/n)"
+if %rdpChk%==y (
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v AllowTSConnections /t REG_DWORD /d 1 /f
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp /t REG_DWORD /d 1 /f
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 1 /f
+	REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+	netsh advfirewall firewall set rule group="remote desktop" new enable=yes
+	echo Please select "Allow connections only from computers running Remote Desktop with Network Level Authentication"
+	start SystemPropertiesRemote.exe /wait
+	echo Enabled remote desktop
+	goto:EOF
+)
+if %rdpChk%==n (
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v AllowTSConnections /t REG_DWORD /d 0 /f
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp /t REG_DWORD /d 0 /f
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+	netsh advfirewall firewall set rule group="remote desktop" new enable=no
+	echo Disabled remote desktop
+	goto:EOF
+)
+goto a
+
+pause
