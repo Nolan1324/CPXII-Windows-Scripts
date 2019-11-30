f5657a5f22d5b97a43a3603cab2761da2e78d2d5
Nolan <nkuza8@gmail.com>
2019-11-30 12:21:11 -0500

    Create Firewall.bat

---
+++ Firewall.bat
@@ -0,0 +1,5 @@
+@echo off
+
+netsh advfirewall set allprofiles state on
+netsh advfirewall firewall add rule name="!CyberPatriotTCP" protocol=TCP dir=out remoteport=21,22,23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotUDP" protocol=UDP dir=out remoteport=21,22,23,161,162,636,3269 action=block
