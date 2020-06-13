2a8ac823b2a16e9e5375e20e1ca323371333f72e
JonathanKarr33 <52431288+JonathanKarr33@users.noreply.github.com>
2020-06-13 10:01:47 -0400

    Update Firewall.bat
    blocked ports 411 and 412

--- Firewall.bat
+++ Firewall.bat
@@ -1,10 +1,10 @@
 @echo off
 
 netsh advfirewall set allprofiles state on
-netsh advfirewall firewall add rule name="!CyberPatriotTCPOut" protocol=TCP dir=out remoteport=23,161,162,636,3269 action=block
-netsh advfirewall firewall add rule name="!CyberPatriotTCPIn" protocol=TCP dir=in remoteport=23,161,162,636,3269 action=block
-netsh advfirewall firewall add rule name="!CyberPatriotUDPOut" protocol=UDP dir=out remoteport=23,161,162,636,3269 action=block
-netsh advfirewall firewall add rule name="!CyberPatriotUDPIn" protocol=UDP dir=in remoteport=23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotTCPOut" protocol=TCP dir=out remoteport=23,161,162,411,412,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotTCPIn" protocol=TCP dir=in remoteport=23,161,162,411,412,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotUDPOut" protocol=UDP dir=out remoteport=23,161,162,411,412,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotUDPIn" protocol=UDP dir=in remoteport=23,161,162,411,412,636,3269 action=block
 
 set /p ftpChk="Is FTP a critical service? (y/n)"
 if %ftpChk%==y ( 
