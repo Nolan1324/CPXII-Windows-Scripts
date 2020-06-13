1d5083d4df8d24bcc15c2b8bcabf6e2564e917f3
JonathanKarr33 <52431288+JonathanKarr33@users.noreply.github.com>
2020-06-13 09:38:00 -0400

    Update Firewall.bat
    added dir=in

--- Firewall.bat
+++ Firewall.bat
@@ -1,7 +1,9 @@
 @echo off
 
 netsh advfirewall set allprofiles state on
-netsh advfirewall firewall add rule name="!CyberPatriotTCP" protocol=TCP dir=out remoteport=21,22,23,161,162,636,3269 action=block
-netsh advfirewall firewall add rule name="!CyberPatriotUDP" protocol=UDP dir=out remoteport=21,22,23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotTCPOut" protocol=TCP dir=out remoteport=21,22,23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotTCPIn" protocol=TCP dir=in remoteport=21,22,23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotUDPOut" protocol=UDP dir=out remoteport=21,22,23,161,162,636,3269 action=block
+netsh advfirewall firewall add rule name="!CyberPatriotUDPIn" protocol=UDP dir=in remoteport=21,22,23,161,162,636,3269 action=block
 
 pause
