edd9955bc73b32e9739a6787aecea68da5199bba
Jonathan Karr <52431288+JonathanKarr33@users.noreply.github.com>
2020-12-10 22:33:28 -0500

    fixed error with "

--- Services.bat
+++ Services.bat
@@ -53,13 +53,13 @@ CALL :Disable "WebClient"
 CALL :Disable "IISADMIN"
 CALL :Disable "LanmanServer"
 CALL :Disable "W3SVC"
-CALL :Disable “iphlpsvc”
-CALL :Disable “iprip”
-CALL :Disable “Spooler”
-CALL :Disable “LanmanServer”
-CALL :Disable “SNMPTRAP”
-CALL :Disable “lmhosts”
-CALL :Disable “TapiSvc”
+CALL :Disable "iphlpsvc"
+CALL :Disable "iprip"
+CALL :Disable "Spooler"
+CALL :Disable "LanmanServer"
+CALL :Disable "SNMPTRAP"
+CALL :Disable "lmhosts"
+CALL :Disable "TapiSvc"
 
 REM Enable core services
 CALL :Enable "EventLog"
